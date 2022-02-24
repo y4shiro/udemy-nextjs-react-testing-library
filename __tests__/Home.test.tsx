864247a7a2024788e5f30f0c00e7b576099de51e
y4shiro <y4shironao@gmail.com>
2022-02-24 13:17:27 +0900

    __tests__/ ディレクトリを作成し、Jest と React-testing-library の動作確認

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Home from '../pages/index';
+
+it('Should render hello text', () => {
+  render(<Home />);
+  expect(screen.getByText('Hello Next.js')).toBeInTheDocument();
+});
