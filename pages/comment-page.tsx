51396263547ee7b9e058f89a755bf6d255b5ec92
y4shiro <y4shironao@gmail.com>
2022-02-25 22:17:12 +0900

    Comment コンポーネントを作成し、コメントページで読み込む。コメントページでは SWR でコメントデータを取得管理する

--- pages/comment-page.tsx
+++ pages/comment-page.tsx
@@ -1,9 +1,30 @@
+import useSWR from 'swr';
+import axios from 'axios';
+
 import Layout from '../components/Layout';
+import Comment from '../components/Comment';
+import { COMMENT } from '../types/Types';
+import { comment } from 'postcss';
+
+const axiosFetcher = async () => {
+  const result = await axios.get<COMMENT[]>(
+    'https://jsonplaceholder.typicode.com/comments/?_limit=10'
+  );
+  return result.data;
+};
 
 const CommentPage: React.VFC = () => {
+  const { data: comments, error } = useSWR('commentsFetch', axiosFetcher);
+
+  if (error) return <span>Error!</span>;
+
   return (
     <Layout title="Comment">
       <p className="text-4xl">comment page</p>
+      <ul>
+        {comments &&
+          comments.map((comment) => <Comment key={comment.id} {...comment} />)}
+      </ul>
     </Layout>
   );
 };
