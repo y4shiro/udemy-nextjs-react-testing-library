f457195fbec546f36a7e86e1de6761672ea1c7b5
y4shiro <y4shironao@gmail.com>
2022-02-26 16:53:52 +0900

    Todos ページを実装

--- pages/task-page.tsx
+++ pages/task-page.tsx
@@ -1,11 +1,51 @@
 import Layout from '../components/Layout';
+import { GetStaticProps } from 'next';
+import useSWR from 'swr';
+import axios from 'axios';
 
-const TaskPage: React.VFC = () => {
+import { getAllTasksData } from '../lib/fetch';
+import { TASK } from '../types/Types';
+
+interface STATICPROPS {
+  staticTasks: TASK[];
+}
+
+const axiosFetcher = async () => {
+  const result = await axios.get<TASK[]>(
+    'https://jsonplaceholder.typicode.com/todos/?_limit=10'
+  );
+  return result.data;
+};
+
+const TaskPage: React.VFC<STATICPROPS> = ({ staticTasks }) => {
+  const { data: tasks, error } = useSWR('todosFetch', axiosFetcher, {
+    fallbackData: staticTasks,
+    revalidateOnMount: true,
+  });
+
+  if (error) return <span>Error!</span>;
+
   return (
     <Layout title="Todos">
-      <p className="text-4xl">todos page</p>
+      <p className="text-4xl mb-10">todos page</p>
+      <ul>
+        {tasks &&
+          tasks.map((task) => (
+            <li key={task.id}>
+              {`${task.id}: `}
+              <span>{task.title}</span>
+            </li>
+          ))}
+      </ul>
     </Layout>
   );
 };
 
 export default TaskPage;
+
+const getStaticProps: GetStaticProps = async () => {
+  const staticTasks = await getAllTasksData();
+  return {
+    props: { staticTasks },
+  };
+};
