3f0cd52fbdddaa17eed4cc261ff460e4e7a482d3
y4shiro <y4shironao@gmail.com>
2022-02-26 16:20:52 +0900

    Context ページ関連のコンポーネント作成と、Context ページ自体を実装

--- pages/context-page.tsx
+++ pages/context-page.tsx
@@ -1,9 +1,17 @@
 import Layout from '../components/Layout';
+import { StateProvider } from '../context/StateProvider';
+import ContextA from '../components/ContextA';
+import ContextB from '../components/ContextB';
 
 const ContextPage: React.VFC = () => {
   return (
     <Layout title="Context">
-      <p className="text-4xl">context page</p>
+      <p className="text-4xl mb-10">context page</p>
+
+      <StateProvider>
+        <ContextA />
+        <ContextB />
+      </StateProvider>
     </Layout>
   );
 };
